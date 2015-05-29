a3c90f614dab754b2379a155121d49a40f63cbd0
Aleksandar Faraj <aleksandarfarajgmail.com>
2015-05-29 21:40:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+#Material Color
+
+Material Color is a javascript color generator. It is helpful when creating designs that follow Google Material Design.
+
+##Usage
+
+###Node:
+    var MaterialColor = require('MaterialColor');
+
+###Browser:
+    <script src="color.js"></script>
+
+
+###Functions:
+
+####getRandomAccent():
+>name: Name of the color
+>index: What index this accent is from
+>accent: The hexadecimal color code
+>light: Indicates if this color is light. It is recommended to match light colors backgrounds with black text.
+    MaterialColor.getRandomAccent();
+    //{ name: 'amber', index: 3, accent: '#ffab00', light: false }
+
+
+####getRandomHue():
+>name: Name of the color
+>index: What index this accent is from
+>accent: The hexadecimal color code
+>light: indicates if this color is light. It is recommended to match light colors backgrounds with black text.
+    MaterialColor.getRandomHue();
+    //{ name: 'yellow', hue: '#fdd835', index: 6, light: false }
+
+
+####getRandomColor():
+>name: Name of the color
+>hue.colors: An array of colors in descending shades.
+>hue.light: Indicates at what index the last light color is at.
+>accent.colors: An array of colors in descending shades.
+>accent.light: Indicates at what index the last light color is at.
+    MaterialColor.getRandomColor();
+    //{ name: 'yellow',
+    //    hue:
+    //     { light: 2,
+    //       colors:
+    //        [ '#fffde7',
+    //          '#fff9c4',
+    //          '#fff59d',
+    //          '#fff176',
+    //          '#ffee58',
+    //          '#ffeb3b',
+    //          '#fdd835',
+    //          '#fbc02d',
+    //          '#f9a825',
+    //          '#f57f17' ] },
+    //    accent:
+    //     { light: 0,
+    //       colors: [ '#ffff8d', '#ffff00', '#ffea00', '#ffd600' ] } }
+
+
---
+++ color.js
@@ -0,0 +1,453 @@
+// Google Color Palette defined: http://www.google.com/design/spec/style/color.html
+var red = {
+    name: "red",
+    hue: {
+        light: 2,
+        colors: [
+            "#FFEBEE",
+            "#FFCDD2",
+            "#EF9A9A",
+            "#E57373",
+            "#EF5350",
+            "#F44336",
+            "#E53935",
+            "#D32F2F",
+            "#C62828",
+            "#B71C1C"
+        ]
+    }, accent: {
+        light: 0,
+        colors: [
+            "#FF8A80",
+            "#FF5252",
+            "#FF1744",
+            "#D50000"
+        ]
+    }
+};
+var pink = {
+    name: "pink",
+    hue: {
+        light: 2,
+        colors: [
+            "#fce4ec",
+            "#f8bbd0",
+            "#f48fb1",
+            "#f06292",
+            "#ec407a",
+            "#e91e63",
+            "#d81b60",
+            "#c2185b",
+            "#ad1457",
+            "#880e4f"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#ff80ab",
+            "#ff4081",
+            "#f50057",
+            "#c51162"
+        ]
+    }
+};
+var purple = {
+    name: "purple",
+    hue: {
+        light: 2,
+        colors: [
+            "#f3e5f5",
+            "#e1bee7",
+            "#ce93d8",
+            "#ba68c8",
+            "#ab47bc",
+            "#9c27b0",
+            "#8e24aa",
+            "#7b1fa2",
+            "#6a1b9a",
+            "#4a148c"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#ea80fc",
+            "#e040fb",
+            "#d500f9",
+            "#aa00ff"
+        ]
+    }
+};
+var deepPurple = {
+    name: "deepPurple",
+    hue: {
+        light: 2,
+        colors: [
+            "#ede7f6",
+            "#d1c4e9",
+            "#b39ddb",
+            "#9575cd",
+            "#7e57c2",
+            "#673ab7",
+            "#5e35b1",
+            "#512da8",
+            "#4527a0",
+            "#311b92"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#b388ff",
+            "#7c4dff",
+            "#651fff",
+            "#6200ea"
+        ]
+    }
+};
+var indigo = {
+    name: "indigo",
+    hue: {
+        light: 2,
+        colors: [
+            "#e8eaf6",
+            "#c5cae9",
+            "#9fa8da",
+            "#7986cb",
+            "#5c6bc0",
+            "#3f51b5",
+            "#3949ab",
+            "#303f9f",
+            "#283593",
+            "#1a237e"
+        ]
+    }, accent: {
+        light: 0,
+        colors: [
+            "#8c9eff",
+            "#536dfe",
+            "#3d5afe",
+            "#304ffe"
+        ]
+    }
+};
+var blue = {
+    name: "blue",
+    hue: {
+        light: 2,
+        colors: [
+            "#E3F2FD",
+            "#BBDEFB",
+            "#90CAF9",
+            "#64B5F6",
+            "#42A5F5",
+            "#2196F3",
+            "#1E88E5",
+            "#1976D2",
+            "#1565C0",
+            "#0D47A1"
+        ]
+    }, accent: {
+        light: 0,
+        colors: [
+            "#82B1FF",
+            "#448AFF",
+            "#2979FF",
+            "#2962FF"
+        ]
+    }
+};
+var lightBlue = {
+    name: "lightBlue",
+    hue: {
+        light: 2,
+        colors: [
+            "#e1f5fe",
+            "#b3e5fc",
+            "#81d4fa",
+            "#4fc3f7",
+            "#29b6f6",
+            "#03a9f4",
+            "#039be5",
+            "#0288d1",
+            "#0277bd",
+            "#01579b"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#80d8ff",
+            "#40c4ff",
+            "#00b0ff",
+            "#0091ea"
+        ]
+    }
+};
+var cyan = {
+    name: "cyan",
+    hue: {
+        light: 2,
+        colors: [
+            "#e0f7fa",
+            "#b2ebf2",
+            "#80deea",
+            "#4dd0e1",
+            "#26c6da",
+            "#00bcd4",
+            "#00acc1",
+            "#0097a7",
+            "#00838f",
+            "#006064"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#84ffff",
+            "#18ffff",
+            "#00e5ff",
+            "#00b8d4"
+        ]
+    }
+};
+var teal = {
+    name: "teal",
+    hue: {
+        light: 2,
+        colors: [
+            "#e0f2f1",
+            "#b2dfdb",
+            "#80cbc4",
+            "#4db6ac",
+            "#26a69a",
+            "#009688",
+            "#00897b",
+            "#00796b",
+            "#00695c",
+            "#004d40"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#a7ffeb",
+            "#64ffda",
+            "#1de9b6",
+            "#00bfa5"
+        ]
+    }
+};
+var green = {
+    name: "green",
+    hue: {
+        light: 2,
+        colors: [
+            "#E8F5E9",
+            "#C8E6C9",
+            "#A5D6A7",
+            "#81C784",
+            "#66BB6A",
+            "#4CAF50",
+            "#43A047",
+            "#388E3C",
+            "#2E7D32",
+            "#1B5E20"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#B9F6CA",
+            "#69F0AE",
+            "#00E676",
+            "#00C853"
+        ]
+    }
+};
+var lightGreen = {
+    name: "lightGreen",
+    hue: {
+        light: 2,
+        colors: [
+            "#f1f8e9",
+            "#dcedc8",
+            "#c5e1a5",
+            "#aed581",
+            "#9ccc65",
+            "#8bc34a",
+            "#7cb342",
+            "#689f38",
+            "#558b2f",
+            "#33691e"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#ccff90",
+            "#b2ff59",
+            "#76ff03",
+            "#64dd17"
+        ]
+    }
+};
+var lime = {
+    name: "lime",
+    hue: {
+        light: 2,
+        colors: [
+            "#f9fbe7",
+            "#f0f4c3",
+            "#e6ee9c",
+            "#dce775",
+            "#d4e157",
+            "#cddc39",
+            "#c0ca33",
+            "#afb42b",
+            "#9e9d24",
+            "#827717"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#f4ff81",
+            "#eeff41",
+            "#c6ff00",
+            "#aeea00"
+        ]
+    }
+};
+var yellow = {
+    name: "yellow",
+    hue: {
+        light: 2,
+        colors: [
+            "#fffde7",
+            "#fff9c4",
+            "#fff59d",
+            "#fff176",
+            "#ffee58",
+            "#ffeb3b",
+            "#fdd835",
+            "#fbc02d",
+            "#f9a825",
+            "#f57f17"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#ffff8d",
+            "#ffff00",
+            "#ffea00",
+            "#ffd600"
+        ]
+    }
+};
+var amber = {
+    name: "amber",
+    hue: {
+        light: 2,
+        colors: [
+            "#fff8e1",
+            "#ffecb3",
+            "#ffe082",
+            "#ffd54f",
+            "#ffca28",
+            "#ffc107",
+            "#ffb300",
+            "#ffa000",
+            "#ff8f00",
+            "#ff6f00"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#ffe57f",
+            "#ffd740",
+            "#ffc400",
+            "#ffab00"
+        ]
+    }
+};
+var orange = {
+    name: "orange",
+    hue: {
+        light: 2,
+        colors: [
+            "#fff3e0",
+            "#ffe0b2",
+            "#ffcc80",
+            "#ffb74d",
+            "#ffa726",
+            "#ff9800",
+            "#fb8c00",
+            "#f57c00",
+            "#ef6c00",
+            "#e65100"
+        ]
+    }, accent: {
+        light: 0,
+        colors: ["#ffd180",
+            "#ffab40",
+            "#ff9100",
+            "#ff6d00"
+        ]
+    }
+};
+var deepOrange = {
+    name: "deepOrange",
+    hue: {
+        light: 2,
+        colors: [
+            "#fbe9e7",
+            "#ffccbc",
+            "#ffab91",
+            "#ff8a65",
+            "#ff7043",
+            "#ff5722",
+            "#f4511e",
+            "#e64a19",
+            "#d84315",
+            "#bf360c"
+        ]
+    }, accent: {
+        light: 0,
+        colors: [
+            "#ff9e80",
+            "#ff6e40",
+            "#ff3d00",
+            "#dd2c00"
+        ]
+    }
+};
+function getRandom(to) {
+    return Math.floor(Math.random() * to);
+}
+function Colors() {
+    this.array = [red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber, orange, deepOrange];
+}
+Colors.prototype.getRandomColor = function () {
+    return this.array[getRandom(this.array.length)];
+};
+Colors.prototype.getRandomHue = function () {
+    var color = this.getRandomColor();
+    var hues = color.hue.colors;
+    var index = getRandom(hues.length);
+    var hue = hues[index];
+    var light = false;
+    if (index <= color.hue.light) {
+        light = true;
+    }
+    return {
+        name: color.name,
+        hue: hue,
+        index:index,
+        light: light
+    };
+};
+Colors.prototype.getRandomAccent = function () {
+    var color = this.getRandomColor();
+    var accents = color.accent.colors;
+    var index = getRandom(accents.length);
+    var accent = accents[index];
+    var light = false;
+    if (index <= color.accent.light) {
+        light = true;
+    }
+    return {
+        name: color.name,
+        index:index,
+        accent: accent,
+        light: light
+    };
+    return this.array[getRandom(this.array.length)];
+};
+var col = new Colors();
+if (module) {
+    module.exports = col;
+} else {
+    MaterialColor = col;
+}
